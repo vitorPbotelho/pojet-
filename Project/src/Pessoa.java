36c6bc4e7705b7fd0f67e9131298daf78af55090
unknown <vitor_f_botelho@saojose.sc.senai.br>
2019-08-05 08:49:55 -0300

    salvar os projetos

---
+++ Project/src/Pessoa.java
@@ -0,0 +1,20 @@
+
+public class Pessoa {
+	
+	private String name;
+	private Integer idade;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getIdade() {
+		return idade;
+	}
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	} 
+	
+}
